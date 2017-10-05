95ef074d2841318986185596d73431b9719ebbac
Griffin Scott <griffinoscott@gmail.com>
2017-10-05 15:08:21 -0700

    added boilerplate

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*----- constants -----*/
+
+
+
+
+/*----- app's state (variables) -----*/
+
+//slots 1-3
+//score/money
+//bet amount
+
+
+/*----- cached element references -----*/
+
+//slots 1-3
+//button/handle
+
+/*----- event listeners -----*/
+
+//handle
+//betting 
+
+/*----- functions -----*/
+
+//init(), render(), spinSlot(), checkWin(), bet()

1b6c23fb4ea842c22ae2285071997b59acd64e41
Griffin Scott <griffinoscott@gmail.com>
2017-10-06 16:41:23 -0700

    made all images except 5 more efficient

--- js/app.js
+++ js/app.js
@@ -7,8 +7,15 @@ $(function(){
 /*----- app's state (variables) -----*/
 
 slots = ['1', '2', '3'];
-var money = 1000;
-var bet = 0;
+symbols = [
+  {key: 'cherry', img: 'http://downloadclipart.org/do-upload/clipart/2016-06/Cute_cherries_clip_art.png' },
+  {key: 'orange', img: 'http://downloadclipart.org/do-upload/clipart/2016-06/Orange_clipart_png.png' },
+  {key: 'lemon', img: 'https://tr1.cbsistatic.com/hub/i/2015/05/07/a6b60bbe-f4ae-11e4-940f-14feb5cc3d2a/lemon09062012.png' },
+  {key: 'pear', img: 'https://tr1.cbsistatic.com/hub/i/2015/05/07/a71cca26-f4ae-11e4-940f-14feb5cc3d2a/pear09062012.png' },
+  {key: 'seven', img: 'http://clipart-library.com/img/688217.png' }
+]
+var money
+var bet
 //bet amount
 
 
@@ -27,42 +34,55 @@ $('button#coinSlot').on('click', insertCoin)
 
 //init(), render(), spinSlot(), checkWin(), bet()
 function init(){
-
-
+  slots = [5,5,5]
+  money = 1000;
+  bet = 0;
   render();
 }
 
 function render(){
   slots.forEach(function(slot,idx){
+    $slot = $(`td.s${idx+1}`)
+    child = $slot.children();
+    console.log(child)
     switch(slot){
       case 1:
-        $(`td.${idx+1}`).html("<img src='http://www.pngall.com/wp-content/uploads/2016/05/Cherry-PNG-Clipart.png' alt='Whoops' height='250' width='250'>")
-        $(`td.${idx+1}`).css('background-color', 'red');
-        // $(`td.${idx+1}`).text('Cherry')
+        $slot.css('background-color', 'red');
+        child.attr("src", symbols[0].img)
+        child.attr("width", 250)
+        child.attr("height", 280)
+        // $(`td.s${idx+1}`).html("<img src='http://downloadclipart.org/do-upload/clipart/2016-06/Cute_cherries_clip_art.png' alt='Whoops' height='280' width='250'>")
+        
         break;
       case 2: 
-        $(`td.${idx+1}`).css('background-color', 'orange');
-        $(`td.${idx+1}`).html("<img src='http://downloadclipart.org/do-upload/clipart/2016-06/Orange_clipart_png.png' alt='Whoops' height='250' width='250'>")
+        $slot.css('background-color', 'orange');
+        child.attr("src", symbols[1].img)
+        child.attr("width", 250)
+        child.attr("height", 280)
         break;
       case 3: 
-        $(`td.${idx+1}`).css('background-color', 'yellow');
-        $(`td.${idx+1}`).html("<img src='https://tr1.cbsistatic.com/hub/i/2015/05/07/a6b60bbe-f4ae-11e4-940f-14feb5cc3d2a/lemon09062012.png' alt='Whoops' height='220' width='220'>")
+        $slot.css('background-color', 'yellow');
+        child.attr("src", symbols[2].img)
+        child.attr("width", 250)
+        child.attr("height", 280)
         break;
       case 4: 
-        $(`td.${idx+1}`).css('background-color', 'green');
-        $(`td.${idx+1}`).html("<img src='https://tr1.cbsistatic.com/hub/i/2015/05/07/a71cca26-f4ae-11e4-940f-14feb5cc3d2a/pear09062012.png' alt='Whoops' height='220' width='220'>")
+        $slot.css('background-color', 'green');
+        child.attr("src", symbols[3].img)
+        child.attr("width", 250)
+        child.attr("height", 280)
         break;
       case 5: 
-        $(`td.${idx+1}`).css('background-color', 'gold');
-        $(`td.${idx+1}`).html("<img src='http://clipart-library.com/img/688217.png' alt='Whoops' height='325' width='325'>")
+        $(`td.s${idx+1}`).css('background-color', 'gold');
+        $(`td.s${idx+1}`).html("<img src='http://clipart-library.com/img/688217.png' alt='Whoops' height='325' width='325'>")
         break;
       case 6:
-        $(`td.${idx+1}`).css('background-color', 'Pink');
-        $(`td.${idx+1}`).text('Uh-Oh');
+        $(`td.s${idx+1}`).css('background-color', 'Pink');
+        $(`td.s${idx+1}`).text('Uh-Oh');
         break;
 
       default:
-        $(`td.${idx+1}`).text(slot)
+        $(`td.s${idx+1}`).text('Something Happened')
     }
     // $(`td.${idx+1}`).text(slot);
   })
@@ -95,8 +115,7 @@ function spinSlots(){
     } else if (num === 12){
       slots[idx] = 5;
     }
-    //slots[idx] = Math.floor(Math.random()*10) + 1
-    })
+  });
   bet -= 1;
   
 //(state[first] && (state[first] === state[second]) && (state[first] === state[third]))

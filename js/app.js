1d61b4e10fce988d47b2a0c4dfbbf3146078c563
Griffin Scott <griffinoscott@gmail.com>
2017-10-13 09:08:53 -0700

    Cleaned up code and added blank README

--- js/app.js
+++ js/app.js
@@ -14,12 +14,7 @@ var sounds = {
   spinning: 'sounds/',
   switch: "sounds/switch.mp3"
 };
-
-
-
-
 /*----- app's state (variables) -----*/
-
 var money;
 var menu;
 var bet;
@@ -28,15 +23,7 @@ var betAm;
 var animating;
 var player = new Audio();
 var bgPlayer = new Audio();
-/*----- cached element references -----*/
-
-
-
-//slots 1-3
-//button/handle
-
 /*----- event listeners -----*/
-
 $('#handle').on('click', spinSlots);
 $('.bet1').on('click', function(){insertCoin(1)});
 $('.bet25').on('click', function(){insertCoin(25)});
@@ -46,28 +33,14 @@ $('#start').on('click', function() {checkMenu('start')});
 $('#instructions').on('click', function() {checkMenu('inst')});
 $('.back').on('click', function() {checkMenu('back')});
 $('.back2').on('click', function() {checkMenu('back')});
-
-
-//betting 
-
 /*----- functions -----*/
-
-//init(), render(), spinSlot(), checkWin(), bet()
 function playSound(name){
   player.src = sounds[name]
   if (name === 'win'){player.volume = 0.4;}
   if (name === 'quack'){player.volume = 0.5;}
   if (name === 'coin'){player.volume = 0.7;}
-
   player.play();
 };
-
-function playMusic(){
-  // bgPlayer.src = 'sounds/bgmusic.mp3';
-  // bgPlayer.volume = 0.3;
-  // bgPlayer.play();
-}
-
 function checkMenu(name){
   if (name === 'start'){
     $('section#main').css('display', 'block');
@@ -90,18 +63,15 @@ function checkMenu(name){
     menu = 1;
   }
 }
-
 function init(){
-  slots = [5,5,5]
+  slots = [5,5,5];
   money = 50;
   bet = 0;
   render();
-  playMusic();
 }
-
 function render(){
   slots.forEach(function(slot,idx){
-    $slot = $(`div.s${idx+1}`)
+    $slot = $(`div.s${idx+1}`);
     child = $slot.children();
     $(`div.s${idx+1}`).css('background-color', 'white');
     switch(slot){
@@ -130,31 +100,22 @@ function render(){
         child.attr("width", 260);
         child.attr("height", 240);
         break;
-      // case 6:       
-      //   $(`div.s${idx+1}`).html("<img src='http://clipart-library.com/img/688217.png' alt='Whoops' height='270' width='280'>")
-      //   break;
 
       default:
         $(`div.s${idx+1}`).css('background-color', 'Pink');
         $(`div.s${idx+1}`).text('Uh-Oh');
         break;
-
     }
-    // $(`td.${idx+1}`).text(slot);
   })
-  $('p.cash').html(`Winnings: `)
-  $('p.winnings').html(`$${money}`)
-  $('p.betM').html(`Bet: `)
-  $('p.betC').html(`$${bet}`)
-  
-
+  $('p.cash').html(`Winnings: `);
+  $('p.winnings').html(`$${money}`);
+  $('p.betM').html(`Bet: `);
+  $('p.betC').html(`$${bet}`);
 }
 
 function randSlots(idx){
-  var num
-  
+  var num;
     num = Math.floor((Math.random()*12) + 1);
-
     if (num < 5) {
       slots[idx] = 1;
     } else if (num >= 5 && num < 8) {
@@ -166,7 +127,6 @@ function randSlots(idx){
     } else if (num === 12){
       slots[idx] = 5;
     }
-
   render();
 }
 
@@ -174,11 +134,10 @@ function flashColor(color){
   $slots = $('div.squares div');
   var interval = setInterval(function(){
     $slots.css('background-color') === 'rgb(255, 255, 255)' ? $slots.css('background-color', color) : $slots.css('background-color', 'white');
-  }, 250)
-
+  }, 250);
   setTimeout(function(){
     clearInterval(interval)
-  }, 3000)
+  }, 3000);
 }
 
 
@@ -199,42 +158,36 @@ function spinSlots(){
     var third = setInterval(function(){
       randSlots(2);
     }, 50)
-
-  setTimeout(function(){ 
-    playSound('switch');
-    clearInterval(first);
-    setTimeout(function(){
-      clearInterval(second);
+    setTimeout(function(){ 
       playSound('switch');
+      clearInterval(first);
       setTimeout(function(){
-        animating = false;
-        clearInterval(third);
-        cheat();
+        clearInterval(second);
         playSound('switch');
-        checkWin();
+        setTimeout(function(){
+          animating = false;
+          clearInterval(third);
+          cheat();
+          playSound('switch');
+          checkWin();
+        }, 1500)
       }, 1500)
-    }, 1500)
-  }, 1500);
-
+    }, 1500);
   }
 }
-
 function checkWin(){
   if((slots[0] === slots[1]) && (slots[1] === slots[2])){
     playSound('win');
     flashColor(symbols[slots[0]-1].color);
     var amount = Math.floor((multi * 1.2) * 5 * slots[0]);
     multi = 1;
-    console.log(amount);
     var time = setInterval(function(){
       money += Math.floor((amount/6));
-      console.log(Math.floor(amount/6))
       render();
-    }, 500)
-    
+    }, 500);
     setTimeout(function(){
       clearTimeout(time)
-    }, 3000)
+    }, 3000);
   }
   if(money === 0 && bet === 0){
     alert('You lose')
@@ -249,10 +202,7 @@ function cheat(){
     }
   render();
 }
-
-
 function insertCoin(num){
-  console.log(num)
   if (money >= num){
   money -= num;
   bet += num;
@@ -261,26 +211,6 @@ function insertCoin(num){
   if (animating) return;
   playSound('coin')
 }
-
-
-
-// function titleAnim(){
-//   $title = $('h1.title');
-//   var interval = setInterval(function(){
-//     $title.css('color') === 'rgb(255, 215, 0)' ? setTimeout(function(){
-//       $title.css('color', 'gray')}, 500) : $title.css('color', 'gold');
-//     console.log('1234')
-//   }, 1000)
-// }
-
 init();
-// titleAnim();
-
-
-
-
-
-
-
 })
 
